e1e9b317a602b3ccd6259fef3cb7f1ad63027f66
Emil Stener <stener99@hotmail.se>
2016-10-14 14:02:25 +0200

    asd

--- string/Program.cs
+++ string/Program.cs
@@ -10,6 +10,39 @@ namespace @string
     {
         static void Main(string[] args)
         {
+            /*
+            //övning1
+            Console.WriteLine("\u0056\u00E4\u0157\u006D\u0064\u00F6\u0020\u0047\u0079\u006D\u0061\u0073\u0069\u0075\u006D");
+            Console.ReadKey();
+
+           
+
+            //övning2
+            Console.WriteLine("\"Hello World\"");
+            Console.ReadKey();
+
+     
+
+            //övning3
+            string y = "hello World";
+            string x = y.ToUpper();
+            Console.WriteLine(x);
+            Console.ReadKey();
+          
+
+            //övning4
+            string y = "hello World";
+            string x = y.ToLower();
+            Console.WriteLine(x);
+            Console.ReadKey();
+              */
+
+            //övning5
+            string World = "Hello world";
+            Console.WriteLine(World.Length);
+            Console.ReadKey();
+               
+
         }
     }
 }
